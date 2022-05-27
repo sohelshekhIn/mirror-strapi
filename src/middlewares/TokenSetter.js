92fee59d64bae1ee7d5b38341daf0d0a7aee76f0
Sohel Shekh <contact.sohelshekh@gmail.com>
2022-05-27 22:03:29 +0530

    Handle for no JWT in cookies

--- src/middlewares/TokenSetter.js
+++ src/middlewares/TokenSetter.js
@@ -1,13 +1,14 @@
 module.exports = () => {
   return async (ctx, next) => {
-    const cookies = ctx.request.header.cookie;
-    let token = cookies
-      .split(";")
-      .find((c) => c.trim().startsWith("jwt="))
-      .split("=")[1];
-    if (token) {
-      // set the token in the request header as Authorization
-      ctx.request.header.authorization = `Bearer ${token}`;
+    const cookies = ctx.request.header.cookie || false;
+    if (cookies) {
+      let token = cookies
+        .split(";")
+        .find((c) => c.trim().startsWith("jwt="))
+        .split("=")[1];
+      if (token) {
+        ctx.request.header.authorization = `Bearer ${token}`;
+      }
     }
     await next();
   };
